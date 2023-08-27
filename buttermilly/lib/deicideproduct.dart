04443199882da050a8f1bc9c24f25f3881018a0b
sannma31 <kasaisyouun2004@gmail.com>
2023-08-27 21:10:06 +0900

    カメラ機能追加

--- buttermilly/lib/deicideproduct.dart
+++ buttermilly/lib/deicideproduct.dart
@@ -1,12 +1,9 @@
-import 'package:buttermilly/account/account.dart';
-import 'package:buttermilly/bottombar.dart';
-import 'package:buttermilly/group/edit_group_screen.dart';
-import 'package:buttermilly/login.dart';
 import 'package:buttermilly/group/chat_screen.dart';
+import 'package:buttermilly/group/edit_group_screen.dart';
 import 'package:buttermilly/group/home_screen.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-
-import 'bottombar.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class MyApp extends StatelessWidget {
   @override
@@ -18,9 +15,20 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       localizationsDelegates: [],
 
-      //home: Screen1(),
-      home: ChatScreen(),
-      //home: LoginPage(),
+      // home: HomeScreen(),
+      home: FutureBuilder(
+        future: availableCameras(),
+        builder: (_, snapshot) {
+          final cameras = snapshot.data;
+          if (cameras == null) {
+            return Container();
+          }
+          return ChatScreen(
+            cameras: cameras,
+          );
+        },
+      ),
+      // home: LoginPage(),
     );
   }
 }
