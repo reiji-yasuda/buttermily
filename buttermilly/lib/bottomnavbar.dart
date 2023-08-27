c92326d9031e2bc8a4d163572ac4fb3facf2b09d
[kusoerooyajikamiyama@gmail.com] <kusoerooyajikamiyama@gmail.com>
2023-08-27 20:20:10 +0900

    fix

--- buttermilly/lib/bottomnavbar.dart
+++ buttermilly/lib/bottomnavbar.dart
@@ -1,5 +1,5 @@
 import 'package:buttermilly/group/home_screen.dart';
-import 'package:buttermilly/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
 import 'account/account.dart';
