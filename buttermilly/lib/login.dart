baae02d0b851a2eb27bb03c92400cf0458af3a86
reiji-yasuda <gtu89384@kwansei.ac.jp>
2023-08-27 10:38:28 +0900

    確認

--- buttermilly/lib/login.dart
+++ buttermilly/lib/login.dart
@@ -1,5 +1,5 @@
 import 'firebase_options.dart';
-import 'package:buttermilly/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
